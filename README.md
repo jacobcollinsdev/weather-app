be132acfd01af9c97df134d88676955585adfa0d
Jacob Collins <jacobc1596@gmail.com>
2020-05-19 17:47:50 +0100

    finished weather app

---
+++ README.md
@@ -0,0 +1,8 @@
+A simple weather app.
+
+Things I've learned:
+1. Use of background images and background-position property.
+2. Reading API documentation.
+3. Understanding fetch() API.
+4. Extrapolating specific data.
+5. Implementing Date objects.
---
+++ script.js
@@ -0,0 +1,52 @@
+const api = {
+    key:"50cded2caa921633e9f136755a3519b6",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchBox = document.querySelector(".search-box");
+
+searchBox.addEventListener('keypress', setQuery);
+
+function setQuery(e){
+    if (e.keyCode == 13){
+        getResults(searchBox.value);
+    } 
+}
+
+function getResults(query){
+    fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`).then(weather => {
+        return weather.json(); //HTTP response
+    }).then(displayResults);
+}
+
+function displayResults(weather){
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.temp');
+    temp.innerHTML = `${weather.main.temp.toFixed(1)}<span>&deg;c</span>`;
+
+    let description = document.querySelector('.weather');
+    description.innerText = `${weather.weather[0].main}`;
+
+    let minMax = document.querySelector('.hi-low');
+    minMax.innerText = `${weather.main.temp_min.toFixed(1)}°c/ ${weather.main.temp_max.toFixed(1)}°c`
+
+}
+
+function dateBuilder(d){
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
